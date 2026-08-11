70172a7d9915bd8b9f803a03d02547358648e626
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-08-11 15:57:25 +0200

    .

--- PipeMessage.hpp
+++ PipeMessage.hpp
@@ -1,4 +1,7 @@
 
-#define MAX_MESSAGE_SIZE 512
+#define MAX_MESSAGE_SIZE 512 // in bytes
 
 const wchar_t PIPE_NAME[] = L"\\\\.\\pipe\\mynamedpipe";
+
+// Protocol:
+// Bidirectional communication of null-terminated ASCII strings.
