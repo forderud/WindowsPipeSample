d1ef811b669149e75e00f90bacc363e9121b341a
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-08-12 10:16:35 +0200

    .

--- PipeClient/Main.cpp
+++ PipeClient/Main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
 
     wprintf(L"Connected to pipe %s\n\n", PIPE_NAME);
 
-    // change to message-read mode
+    // change to "MESSAGE" read mode to avoid message partial reads
     DWORD mode = PIPE_READMODE_MESSAGE;
     BOOL success = SetNamedPipeHandleState(pipe.get(),
         &mode,    // new pipe mode 
